1599d8af143d6e79e85fc71d873f16f4f02b63e3
Benefrancis <benefrancis@gmail.com>
2023-08-17 11:07:38 -0300

    Estamos implementando a classe ProdutoEstocadoView

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,53 +1,18 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.service.ProdutoEstocadoService;
+import br.com.fiap.domain.entity.ProdutoEstocado;
+import br.com.fiap.domain.view.ProdutoEstocadoView;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        List<Produto> produtos = ProdutoRepository.findAll();
-//
-//        for (Produto produto : produtos) {
-//            System.out.println( produto );
-//        }
+        ProdutoEstocadoView view = new ProdutoEstocadoView();
+        List<ProdutoEstocado> produtoEstocados = view.formEstocar();
 
-//        Produto prod = ProdutoRepository.findById( 8L );
-//
-//        System.out.println(prod);
-
-//        List<Produto> produtos = ProdutoRepository.findByName( "ipad" );
-//
-//        produtos.forEach( System.out::println );
-
-//        ProdutoView view = new ProdutoView();
-//        Produto produto = view.form();
-//        var p = ProdutoService.persist(produto);
-//        if(Objects.nonNull(p)){
-//            System.out.println(p);
-//        }else{
-//            System.out.printf("Não foi possível salvar o produto");
-//        }
-
-//        DepositoView view = new DepositoView();
-//        Deposito deposito = view.form();
-//
-//        var d = DepositoService.persist( deposito );
-//
-//        if (Objects.isNull( d )) {
-//            System.out.println( "Não foi possível salvar o Depósito" );
-//        } else {
-//            System.out.println( d );
-//        }
-
-
-        //Testando o método que gera o dígito
-//        var i = 0;
-//        do {
-//            var digito = ProdutoEstocadoService.gerarDigito( i );
-//            i++;
-//        } while (i < 100);
-
+        produtoEstocados.forEach( System.out::println );
 
     }
 }
--- src/main/java/br/com/fiap/domain/view/ProdutoEstocadoView.java
+++ src/main/java/br/com/fiap/domain/view/ProdutoEstocadoView.java
@@ -5,6 +5,7 @@ import br.com.fiap.domain.entity.Produto;
 import br.com.fiap.domain.entity.ProdutoEstocado;
 import br.com.fiap.domain.repository.DepositoRepository;
 import br.com.fiap.domain.repository.ProdutoRepository;
+import br.com.fiap.domain.service.ProdutoEstocadoService;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -17,9 +18,12 @@ public class ProdutoEstocadoView {
     List<ProdutoEstocado> produtosEstocados = new ArrayList<>();
 
     public List<ProdutoEstocado> formEstocar() {
+
         produtos = ProdutoRepository.findAll();
         depositos = DepositoRepository.findAll();
+
         if (produtos.size() > 0 && depositos.size() > 0) {
+
             Produto produto = (Produto) JOptionPane.showInputDialog( null, "Escolha o produto", "Seleção de Produtos", JOptionPane.QUESTION_MESSAGE, null, produtos.toArray(), produtos.get( 0 ) );
             if (Objects.isNull( produto )) return produtosEstocados;
 
@@ -27,9 +31,20 @@ public class ProdutoEstocadoView {
             if (Objects.isNull( deposito )) return produtosEstocados;
 
             Integer quantidade = 0;
+
             do {
                 quantidade = Integer.valueOf( JOptionPane.showInputDialog( "Informe a quantidade de Itens do produto " + produto.getNome() + " que deseja encaminhar para o estoque" ) );
             } while (quantidade <= 0);
+
+
+            for (int i = 0; i < quantidade; i++) {
+                ProdutoEstocado pe = new ProdutoEstocado();
+                pe.setProduto( produto ).setDeposito( deposito );
+                produtosEstocados.add( ProdutoEstocadoService.entrada( pe ) );
+            }
+
+            if (produtosEstocados.size() > 0)
+                JOptionPane.showMessageDialog( null, "Sucesso!\n" + produtosEstocados.size() + " intens de " + produto.getNome() + " foram estocados com sucesso em " + deposito.getNome() + "!" );
 
         } else {
             JOptionPane.showMessageDialog( null, "Não é possível estocar. Verifique se existem produtos e depósitos disponíveis " );
